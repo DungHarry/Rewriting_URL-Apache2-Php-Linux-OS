516e963d95118fa05d368f53afbd60b419b6d499
Dung Harry <lyvietdung92@gmail.com>
2013-10-23 17:30:42 +0700

    Rewriting url with apache2 and php on Linux Operating System

---
+++ var/www/ProgrammingGroup/product.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Product php</title>
+</head>
+
+<body>
+<?php
+	echo "Product id: " . $_GET["pid"] . "!" . "<br />";
+	echo "Time: " .$_GET["time"] . "!";
+?>
+</body>
+</html>
